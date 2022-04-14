f22f22c4505f783b4b5129310984c3fab0e73a1d
HunterChan <1480811935@qq.com>
2022-04-14 16:16:55 +0800

    新增员工信息分页查询

--- src/main/java/com/demo/reggie/controller/EmployeeController.java
+++ src/main/java/com/demo/reggie/controller/EmployeeController.java
@@ -1,16 +1,15 @@
 package com.demo.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.demo.reggie.common.R;
 import com.demo.reggie.entity.Employee;
 import com.demo.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
@@ -86,4 +85,15 @@ public class EmployeeController {
         employeeService.save(employee);
         return R.success("保存成功");
     }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        Page pageInfo = new Page(page, pageSize);
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.hasLength(name), Employee::getName, name);
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+        employeeService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+    }
+
 }
