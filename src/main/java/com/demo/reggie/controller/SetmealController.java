abffc4897d641fb7fb73b23717bd58602581b025
HunterChan <1480811935@qq.com>
2022-04-19 15:10:45 +0800

    新增套餐分页查询

--- src/main/java/com/demo/reggie/controller/SetmealController.java
+++ src/main/java/com/demo/reggie/controller/SetmealController.java
@@ -1,15 +1,21 @@
 package com.demo.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.demo.reggie.common.R;
 import com.demo.reggie.dto.SetmealDto;
+import com.demo.reggie.entity.Category;
+import com.demo.reggie.entity.Setmeal;
+import com.demo.reggie.service.CategoryService;
 import com.demo.reggie.service.SetmealDishService;
 import com.demo.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Hunter Chen
@@ -22,11 +28,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class SetmealController {
     @Autowired
     private SetmealService setmealService;
-
+    @Autowired
+    private CategoryService categoryService;
 
     @PostMapping
     public R<String> save(@RequestBody SetmealDto setmealDto) {
         setmealService.saveWithDish(setmealDto);
         return R.success("添加成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        Page<Setmeal> pageInfo = new Page<>(page, pageSize);
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+        LambdaQueryWrapper<Setmeal> lambdaQueryWrapper = new LambdaQueryWrapper();
+        lambdaQueryWrapper.like(name != null, Setmeal::getName, name);
+        lambdaQueryWrapper.orderByDesc(Setmeal::getUpdateTime);
+        setmealService.page(pageInfo, lambdaQueryWrapper);
+        BeanUtils.copyProperties(pageInfo, setmealDtoPage);
+        List<Setmeal> records = pageInfo.getRecords();
+        List<SetmealDto> list = setmealDtoPage.getRecords();
+        list = records.stream().map((item) -> {
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item, setmealDto);
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            if (category != null) {
+                setmealDto.setCategoryName(category.getName());
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+        setmealDtoPage.setRecords(list);
+        return R.success(setmealDtoPage);
     }
 }
