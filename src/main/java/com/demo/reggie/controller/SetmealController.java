d0672ebaebc335fa53130730edfe9738cec05e6f
HunterChan <1480811935@qq.com>
2022-04-25 16:12:58 +0800

    套餐采用spring cache缓存

--- src/main/java/com/demo/reggie/controller/SetmealController.java
+++ src/main/java/com/demo/reggie/controller/SetmealController.java
@@ -13,6 +13,7 @@ import com.demo.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +35,7 @@ public class SetmealController {
     private CategoryService categoryService;
 
     @PostMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto) {
         setmealService.saveWithDish(setmealDto);
         return R.success("添加成功");
@@ -65,6 +67,7 @@ public class SetmealController {
     }
 
     @DeleteMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids) {
         setmealService.deleteWithDish(ids);
         return R.success("删除成功");
@@ -115,7 +118,8 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
-    @Cacheable(cacheNames = "setmealCache", key = "#setmeal.categoryId + '_' + #setmeal.status", unless = "#result == null") //  在 R 中 implements Serializable
+    @Cacheable(cacheNames = "setmealCache", key = "#setmeal.categoryId + '_' + #setmeal.status", unless = "#result == null")
+    //  在 R 中 implements Serializable
     public R<List<Setmeal>> list(Setmeal setmeal) {
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId() != null, Setmeal::getCategoryId, setmeal.getCategoryId());
